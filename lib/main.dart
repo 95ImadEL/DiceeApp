27fcb5da5a5e77dda94a818e2850e5923ccfc718
Imad <imad.elbouazzaoui.ee@gmail.com>
2022-12-24 20:28:54 +0100

    Randomising both dices

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class _DicePageState extends State<DicePage> {
                 onPressed: () {
                   setState(() {
                     leftDiceNumber = Random().nextInt(6) + 1;
+                    rightDiceNumber = Random().nextInt(6) + 1;
                   });
                 },
                 child: Image.asset("images/dice$leftDiceNumber.png"),
@@ -52,6 +53,7 @@ class _DicePageState extends State<DicePage> {
               child: TextButton(
                 onPressed: () {
                   setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
                     rightDiceNumber = Random().nextInt(6) + 1;
                   });
                 },
