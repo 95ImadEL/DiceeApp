77290e88286f6df554d24e1370b38a61cde152d9
Imad <imad.elbouazzaoui.ee@gmail.com>
2022-12-24 18:35:48 +0100

    Use the expanded widget to create flexbile layouts

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
@@ -18,6 +19,15 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          child: Image.asset("images/dice1.png"),
+        ),
+        Expanded(
+          child: Image.asset("images/dice3.png"),
+        )
+      ],
+    );
   }
 }
